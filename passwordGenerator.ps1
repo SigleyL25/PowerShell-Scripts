9148b8e02c5e3414c2c8d08342f01ebb71e56529
Larry Sigley <sigleyl25@gmail.com>
2018-01-23 20:54:12 -0800

    Add password generator script

---
+++ passwordGenerator.ps1
@@ -0,0 +1,12 @@
+﻿#random password generator
+
+$passwordChoices = "Abz=yYCDGejhlkmnOPqtt$%^34434478DFhi<$"
+
+#change the 11 to any number for max length/limit of password
+$storeRandomChr=for ($i=0; $i -lt 11; $i++) {$passwordChoices[(Get-Random -Maximum $passwordChoices.length)]} 
+
+#takes each vertical chr and joins it so there is no spaces and not horizontal
+$newPassword = $storeRandomChr -join "" 
+
+#debug purposes
+Write-Host  $newPassword -ForegroundColor Yellow
