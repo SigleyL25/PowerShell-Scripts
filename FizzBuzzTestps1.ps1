6dbe86b3c3776323657eb5502cfafa5c0c18af7b
Larry Sigley <sigleyl25@gmail.com>
2018-01-23 20:41:40 -0800

    Added FizzBuzz test

---
+++ FizzBuzzTestps1.ps1
@@ -0,0 +1,16 @@
+﻿for ([int]$i = 1; $i -le 100; $i++){
+
+       if ($i % 3 -eq 0){
+
+            Write-Host "Fizz $i "
+
+       } elseif ($i % 5 -eq 0) {
+
+            Write-Host "Buzz $i " 
+     
+       } else {
+
+            Write-Host "FizzBuzz $i "
+       }
+
+}
